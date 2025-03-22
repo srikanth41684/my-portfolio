0530f29f61ab7b4b0e5e8beb515bff076a57e9e9
srikanth41684 <srikanthusnagiri41684@gmail.com>
2025-03-22 16:37:28 +0530

    home page design with resume

--- components/home/homePage.js
+++ components/home/homePage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { homePageStyles } from "./homePageStyles";
 import { globalStyles } from "@/styles/globalStyles";
+import { TypeAnimation } from "react-type-animation";
+import Image from "next/image";
+// import resume from "@/public/Srikanth_Usnagiri.pdf";
 
 const homePage = () => {
   return (
@@ -8,7 +11,65 @@ const homePage = () => {
       <style jsx>{globalStyles}</style>
       <style jsx>{homePageStyles}</style>
       <div className={"commonContainer"}>
-        <div className={"homeMainContainer"}>homePage</div>
+        <div className={"homeMainContainer"}>
+          <div className={"titlesContainer"}>
+            <div className={"nameTitle"}>Hello, I'm Srikanth Usnagiri</div>
+            <div className={"jobTitle"}>
+              I'm a{" "}
+              <TypeAnimation
+                sequence={[
+                  "Frontend Engineer",
+                  1000,
+                  "React Developer",
+                  1000,
+                  "Frontend Developer",
+                  1000,
+                  "",
+                  100,
+                ]}
+                style={{ color: "#ffff00" }}
+                speed={50}
+                repeat={Infinity} // Infinite loop
+              />
+            </div>
+          </div>
+          <div className={"ButtonsContainer"}>
+            <div className={"socilaMediaContainer"}>
+              <div className={"socialIconsParent"}>
+                <a
+                  href="https://www.linkedin.com/in/srikanth-usnagiri-042484213"
+                  target="_blank"
+                >
+                  <Image
+                    src={"https://vishalsawai.netlify.app/assets/linkedin.svg"}
+                    width={40}
+                    height={40}
+                    alt="linkIn"
+                    className={"socialIconIn"}
+                  />
+                </a>
+              </div>
+              <div className={"socialIconsParent"}>
+                <a href="https://github.com/srikanth41684" target="_blank">
+                  <Image
+                    src={"https://vishalsawai.netlify.app/assets/github.svg"}
+                    width={40}
+                    height={40}
+                    alt="linkIn"
+                    className={"socialIcon"}
+                  />
+                </a>
+              </div>
+            </div>
+            <a
+              href={"/Srikanth_Usnagiri.pdf"}
+              target="_blank"
+              className={"resumeBtnContainer"}
+            >
+              <div className={"resumeBtn"}>Resume</div>
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
--- components/home/homePageStyles.js
+++ components/home/homePageStyles.js
@@ -5,6 +5,74 @@ export const homePageStyles = css`
     max-width: 1366px;
     margin: 0 auto;
     padding: 30px;
-    height: calc(100vh - 79px);
+    height: calc(100vh - 78px);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 50px;
+  }
+
+  .titlesContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+  }
+
+  .nameTitle {
+    color: var(--text-color);
+    font-size: 42px;
+    line-height: 32px;
+    font-weight: 700;
+  }
+
+  .jobTitle {
+    color: var(--text-color);
+    font-size: 32px;
+    line-height: 25px;
+    font-weight: 700;
+  }
+
+  .ButtonsContainer {
+    display: flex;
+    align-items: center;
+    gap: 50px;
+  }
+
+  .socilaMediaContainer {
+    display: flex;
+    gap: 20px;
+  }
+
+  .socialIconsParent {
+    background-color: #0e2335;
+    border-radius: 4px;
+    width: 45px;
+    height: 45px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+
+  :global(.socialIcon) {
+    padding: 8px;
+  }
+
+  .resumeBtnContainer {
+    text-decoration: none;
+  }
+
+  .resumeBtn {
+    cursor: pointer;
+    background-color: #0e2335;
+    border-radius: 8px;
+    text-align: center;
+    padding: 18px 30px;
+    font-size: 16px;
+    line-height: 12px;
+    color: #fff;
+    font-weight: 600;
   }
 `;
