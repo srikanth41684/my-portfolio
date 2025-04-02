825d297fed73a595e0115c099405c1d0ae550d1a
srikanth41684 <srikanthusnagiri41684@gmail.com>
2025-04-02 19:47:00 +0530

    project design

--- components/projects/projectPage.js
+++ components/projects/projectPage.js
@@ -1,8 +1,9 @@
 import React from "react";
 import dwelloWeb from "@/public/projects/dwello.png";
-import ParallaxProjectCard from "../parallaxProjectCard/parallaxProjectCard";
 import { projectPageStyles } from "./projectPageStyles";
 import Image from "next/image";
+import { globalStyles } from "@/styles/globalStyles";
+import Link from "next/link";
 
 const ProjectPage = () => {
   const projects = [
@@ -24,37 +25,59 @@ const ProjectPage = () => {
       technologies: ["nextjs", "javascript", "reactjs", "styled-jsx", "axios"],
       image: dwelloWeb,
     },
+    {
+      id: 3,
+      title: "Dwello",
+      about:
+        "Dwello is a real estate platform that helps users buy, sell, and explore properties with detailed insights. It offers property listings with real-time updates, advanced search filters, and location-based recommendations. The website is designed to provide a seamless experience for home buyers by integrating interactive maps, virtual property tours, and expert guidance. It connects buyers with verified developers and agents, ensuring a transparent and hassle-free property search. The platform focuses on user-friendly navigation, fast-loading pages, and mobile responsiveness, making real estate transactions easier and more efficient.",
+      url: "https://dwello.in",
+      technologies: ["nextjs", "javascript", "reactjs", "styled-jsx", "axios"],
+      image: dwelloWeb,
+    },
   ];
   return (
     <>
+      <style jsx>{globalStyles}</style>
       <style jsx>{projectPageStyles}</style>
-      <div className={"projectsSection"}>
-        <h2 className={"title"}>Projects</h2>
-        <div className={"projectsContainer"}>
-          {projects.map((project, index) => (
-            <div>
-              <div>
-                <div>
-                  <div>{project?.title}</div>
-                  <div>{project?.about}</div>
-                </div>
-                <div>
-                  <div>Live view</div>
-                  {project?.technologies?.map((tech, index) => (
-                    <div key={index}>{tech}</div>
-                  ))}
+      <div className={"commonContainer"}>
+        <div className={"projectsSection"}>
+          <div className={"mainTitle projectMainTitle"}>Project</div>
+          <hr className="hrLine" />
+          <div className={"projectsContainer"}>
+            {projects.map((project, index) => (
+              <div className={"projectFullContainer"}>
+                <div className={"projectContainer"}>
+                  <div className={"projectTitleContainer"}>
+                    <div className={"aboutProjectContainer"}>
+                      <div className={"projectTitle"}>{project?.title}</div>
+                      <div className={"projectAbout"}>{project?.about}</div>
+                    </div>
+                    <div classNmae={"projectViewContainer"}>
+                      <Link href={project?.url} target="_blank">
+                        <div className={"projectView"}>Live view</div>
+                      </Link>
+                      <div className={"techContainer"}>
+                        {project?.technologies?.map((tech, index) => (
+                          <div className={"tectTitle"} key={index}>
+                            {tech}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                  <div className={"projectImageContainer"}>
+                    <Image
+                      src={project?.image}
+                      className={"projectImage"}
+                      width={400}
+                      height={400}
+                      alt={project?.title}
+                    />
+                  </div>
                 </div>
               </div>
-              <div>
-                <Image
-                  src={project?.image}
-                  width={300}
-                  height={200}
-                  alt={project?.title}
-                />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
     </>
--- components/projects/projectPageStyles.js
+++ components/projects/projectPageStyles.js
@@ -2,11 +2,19 @@ import css from "styled-jsx/css";
 
 export const projectPageStyles = css`
   .projectsSection {
-    padding: 100px 20px;
-    background: var(--bg-color);
-    color: white;
-    text-align: center;
-    min-height: 200vh;
+    max-width: 1366px;
+    margin: 0 auto;
+    padding: 65px 30px 30px;
+  }
+
+  .projectMainTitle {
+    position: sticky;
+    top: 80px;
+  }
+
+  .hrLine {
+    position: sticky;
+    top: 130px;
   }
 
   .title {
@@ -15,10 +23,112 @@ export const projectPageStyles = css`
   }
 
   .projectsContainer {
-    display: flex;
+    position: relative;
+  }
+
+  .projectFullContainer {
     flex-direction: column;
     align-items: center;
-    position: relative;
-    height: 100vh;
+    height: 81vh;
+    display: flex;
+    justify-content: center;
+    position: sticky;
+    top: 132px;
+  }
+
+  .projectContainer {
+    display: flex;
+    gap: 30px;
+    background: #ffffff0d;
+    max-width: 999px;
+    border-radius: 20px;
+    overflow: hidden;
+    transition: background-color 0.3s ease;
+  }
+
+  .projectContainer:hover {
+    background: #ffffff20;
+  }
+
+  .projectTitleContainer {
+    display: flex;
+    padding: 40px 5px 30px 40px;
+    flex-direction: column;
+    gap: 20px;
+    width: 150%;
+  }
+
+  .aboutProjectContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+
+  .projectTitle {
+    font-size: 22px;
+    text-transform: uppercase;
+    font-weight: 600;
+    line-height: 25px;
+    background: linear-gradient(to right, #f32170, #9333ea);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-size: 100% 100%;
+    background-position: center;
+    display: inline-block;
+  }
+
+  .projectAbout {
+    font-size: 14px;
+    line-height: 21px;
+    color: #cbd5e1;
+    font-weight: 400;
+  }
+
+  .projectViewContainer {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .projectView {
+    border: 1px solid #f32170;
+    border-radius: 5px;
+    font-size: 16px;
+    color: #cbd5e1;
+    padding: 8px 15px;
+    display: inline-flex;
+    cursor: pointer;
+  }
+
+  .techContainer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+    padding-top: 10px;
+  }
+
+  .tectTitle {
+    background: #be123c;
+    border-radius: 25px;
+    padding: 5px 15px;
+    display: inline-flex;
+    color: #cbd5e1;
+    font-size: 12px;
+    line-height: 15px;
+    font-weight: 400;
+    text-transform: uppercase;
+  }
+
+  .projectImageContainer {
+    display: flex;
+    align-items: flex-end;
+    width: 40%;
+  }
+
+  :global(.projectImage) {
+    width: 400px;
+    height: 78%;
+    object-fit: cover;
+    object-position: left;
   }
 `;
